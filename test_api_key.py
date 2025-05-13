4d7da9c66d636c40b4f507717c9a7b1bb613fffb
JingmingLiu <jingmingliu01@outlook.com>
2025-05-13 04:20:06 -0400

    Add functionality to test OpenAI and Together models
    - Enhanced `test_api_key.py` to list and print models from both OpenAI and Together APIs.

--- test_api_key.py
+++ test_api_key.py
@@ -1,8 +1,35 @@
+# OpenAI Models
 from openai import OpenAI
 
 client = OpenAI()
 
 models = client.models.list()
 
+print("--------------------------------")
+print("--------OpenAI Models:----------")
+print("--------------------------------")
 for model in models:
     print(model.id)
+print("--------------------------------")
+print("--------------------------------")
+
+
+# Together Models
+import requests
+
+print("--------------------------------")
+print("--------Together Models:---------")
+print("--------------------------------")
+
+url = "https://api.together.xyz/v1/models"
+
+headers = {
+    "accept": "application/json",
+    "authorization": "Bearer cfb73dca729628106d333426a418ca08ace664ad6f0512e31c130d3b765240ff"
+}
+
+response = requests.get(url, headers=headers)
+for model in response.json():
+    print(model["id"])
+print("--------------------------------")
+print("--------------------------------")
